31563b5b625e70b4b431a9d6cdce815cf4df25f4
terry <mingyteng@163.com>
2020-12-31 18:34:05 +0800

    thinking in java chapter21 concurrent cyclicBarrier

--- src/java/thinkinginjava/chapter21/other/CyclicBarrierDemo.java
+++ src/java/thinkinginjava/chapter21/other/CyclicBarrierDemo.java
@@ -1,2 +1,100 @@
-package thinkinginjava.chapter21.other;public class CyclicBarrierDemo {
+package thinkinginjava.chapter21.other;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+
+// CyclicBarrier的构造器需要传入一个int的计数值值和一个Runnable（称作barrierCommand）
+// 每调用一次 await()，计数值就会减1，如果计数值仍然大于0，则挂起
+// 否则就会执行barrierCommand（如果有的话），并且重制计数值，进行下一次循环的计数
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        new HorseRace(7, 200);
+    }
+}
+
+class Horse implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private int strides = 0;
+    private static final Random random = new Random(47);
+    private final CyclicBarrier barrier;
+
+    public Horse(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    strides += random.nextInt(3);
+                }
+                barrier.await();
+            }
+        } catch (InterruptedException ignored) {
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Horse " + id;
+    }
+
+    public synchronized int getStrides() {
+        return strides;
+    }
+
+    public String tracks() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < getStrides(); i++) {
+            sb.append("*");
+        }
+        sb.append(id);
+        return sb.toString();
+    }
+}
+
+class HorseRace {
+    static final int FINISH_LINE = 75;
+    private final List<Horse> horses = new ArrayList<>();
+    private final ExecutorService exec = Executors.newCachedThreadPool();
+    private final CyclicBarrier barrier;
+    public HorseRace(int nHorses, final int pause) {
+        barrier = new CyclicBarrier(nHorses, new Runnable() {
+            @Override
+            public void run() {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < FINISH_LINE; i++) {
+                    sb.append("=");
+                }
+                System.out.println(sb);
+                for (Horse horse : horses) {
+                    System.out.println(horse.tracks());
+                }
+                for (Horse horse : horses) {
+                    if (horse.getStrides() >= FINISH_LINE) {
+                        System.out.println(horse + " won!");
+                        exec.shutdownNow();
+                        return ;
+                    }
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(pause);
+                } catch (InterruptedException e) {
+                    System.out.println("Barrier-action sleep interrupted");
+                }
+            }
+        });
+        for (int i = 0; i < nHorses; i++) {
+            Horse horse = new Horse(barrier);
+            horses.add(horse);
+            exec.execute(horse);
+        }
+    }
 }
