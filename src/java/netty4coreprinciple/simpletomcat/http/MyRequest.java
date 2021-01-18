c98fd489d0ebdd38255691a57ed56c571b7229ee
terry <mingyteng@163.com>
2021-01-19 00:36:58 +0800

    Netty 4 Core Principle Simple Tomcat based on classic I/O

--- src/java/netty4coreprinciple/simpletomcat/http/MyRequest.java
+++ src/java/netty4coreprinciple/simpletomcat/http/MyRequest.java
@@ -6,6 +6,20 @@ public class MyRequest {
     private String method;
     private String url;
 
+    /***
+     *  HTTP请求格式：（从Chrome中截取部分）
+        我们只需要请求中的方法以及url
+
+     GET /firstServlet.do HTTP/1.1
+     Host: localhost:8080
+     Connection: keep-alive
+     Cache-Control: max-age=0
+     sec-ch-ua: "Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"
+     sec-ch-ua-mobile: ?0
+     Upgrade-Insecure-Requests: 1
+     User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
+     Accept: text/html,application/xhtml+xml,application/xml;
+     ***/
     public MyRequest(InputStream in) {
         try {
             String content = "";
